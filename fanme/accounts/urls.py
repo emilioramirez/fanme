1e29fae6493bedbe5ba28ae7405495abd476dd2d
Matias Mu?oz <munozmatiasn@gmail.com>
2011-07-28 17:23:14 -0300

    Agregue la pagina thanks. Y modifique el archivos .css: accounts-thanks y accoutns-thanks-welcome-image

--- fanme/accounts/urls.py
+++ fanme/accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     (r'^user/', views.register_user),
     (r'^company/', views.register_company),
     (r'^login/', views.login_user),
+    (r'^thanks/', views.thanks),    
 )
