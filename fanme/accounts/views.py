2e5545b4a21008ce6776dcbced39c7002cedae92
Emilio Ramirez <emilioramirez04@gmail.com>
2011-07-28 16:00:12 -0300

    terminado el registro de usuario y empresa, agregado nueva app de soporte

--- fanme/accounts/views.py
+++ fanme/accounts/views.py
@@ -1,21 +1,20 @@
 # Views
-from django.shortcuts import render_to_response
-#from django.core.context_processors import csrf
-from django.template import RequestContext
-from fanme.accounts.forms import UserRegisterForm
 from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from fanme.accounts.models import Persona
+#from django.contrib.auth.forms import AuthenticationForm
+from fanme.accounts.forms import UserRegisterForm, CompanyRegisterForm, UserLogin
+from fanme.accounts.models import Persona, Empresa
+from fanme.soport.models import Rubro
 
 
-def dashboard(request):
-    return render_to_response('base.html', context_instance=RequestContext(request))
-
-
-def register(request):
+def register_user(request):
     if request.method == 'POST': # If the form has been submitted...
-        form = UserRegisterForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
+        form_register = UserRegisterForm(request.POST) # A form bound to the POST data
+        form_login = UserLogin(request.POST) # A form bound to the POST data
+        if form_register.is_valid(): # All validation rules pass
             # Process the data in form.cleaned_data
             username = form.cleaned_data['username']
             birth_date = form.cleaned_data['birth_date']
@@ -29,10 +28,60 @@ def register(request):
             profile.sexo = sex
             user.save()
             profile.save()
-            return HttpResponseRedirect('/dashboard/') # Redirect after POST
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+        #~ elif form_login.is_valid(): # All validation rules pass
+            #~ return HttpResponseRedirect('/dashboard/') # Redirect after POST
     else:
-        form = UserRegisterForm() # An unbound form
+        form_register = UserRegisterForm() # An unbound form
+        form_login = UserLogin() # An unbound form
 
-    return render_to_response('accounts/register_form.html', {'form': form, },
+    return render_to_response('accounts/register_user_form.html',
+                            {'form_register': form_register, 'form_login': form_login },
+                            context_instance=RequestContext(request))
+
+def register_company(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = CompanyRegisterForm(request.POST) # A form bound to the POST data
+        form_login = UserLogin(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            username = form.cleaned_data['username']
+            razon_social = form.cleaned_data['razon_social']
+            direccion = form.cleaned_data['direccion']
+            rubro = form.cleaned_data['rubro']
+            url = form.cleaned_data['url']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(username, email, password)
+            profile = Empresa()
+            profile.user = user
+            profile.razon_social = razon_social
+            profile.site = url
+            user.save()
+            profile.save()
+            profile.rubro.add(Rubro.objects.get(nombre=rubro))
+            profile.save()
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = CompanyRegisterForm() # An unbound form
+        form_login = UserLogin() # An unbound form
+
+    return render_to_response('accounts/register_company_form.html',
+                             {'form': form, 'form_login': form_login },
                               context_instance=RequestContext(request))
 
+def login_user(request):
+    username = request.POST['login_username']
+    password = request.POST['login_password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Redirect to a success page.
+            return HttpResponseRedirect('/dashboard/') # Redirect after POST
+        else:
+            # Return a 'disabled account' error message
+            return HttpResponseRedirect('/disable/')
+    else:
+        # Return an 'invalid login' error message.
+        return HttpResponseRedirect('/error/')
