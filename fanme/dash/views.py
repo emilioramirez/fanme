0ddd62187f51843d0c2f2ca4d8bc2dab23552f51
Emilio Ramirez <emilioramirez04@gmail.com>
2011-08-16 16:23:46 -0300

    some modifications

--- fanme/dash/views.py
+++ fanme/dash/views.py
@@ -1,43 +1,55 @@
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+
 from fanme.dash.forms import SearchBox
+from fanme.support.models import Item
 from fanme.accounts.models import Persona
 
 
 @login_required(login_url='/accounts/user/')
 def dashboard(request):
     searchbox = SearchBox()
-    #~ if request.user.is_authenticated():
-        #~ profile = Persona.objects.get(user=request.user)
-        #~ birt = profile.fecha_nacimiento
-    #~ else:
-        #~ # Do something for anonymous users.
-        #~ pass
-    return render_to_response('dash/dashboard.html',
-                            {'form_search': searchbox},
-                            context_instance=RequestContext(request))
+    try:
+        user = User.objects.get(id=request.user.id)
+        profile = user.persona
+        items_by_topics = []
+        item_topic = {}
+        for topic in profile.topicos.all():
+            item_topic[topic] = Item.objects.filter(topico__exact=topic)
+            items_by_topics.append(Item.objects.filter(topico__exact=topic))
+    except Persona.DoesNotExist:
+        return HttpResponseRedirect('/dash/empresa/')
+    return render_to_response('dash/dashboard.html', {'form_search': searchbox,
+        'profile': profile, 'items': items_by_topics, 'example': item_topic},
+        context_instance=RequestContext(request))
 
 
 @login_required(login_url='/accounts/user/')
 def logbook(request):
     searchbox = SearchBox()
-    return render_to_response('dash/logbook.html',
-                            {'form_search': searchbox},
-                            context_instance=RequestContext(request))
+    return render_to_response('dash/logbook.html', {'form_search': searchbox},
+        context_instance=RequestContext(request))
 
 
 @login_required(login_url='/accounts/user/')
 def topicos(request):
     searchbox = SearchBox()
-    return render_to_response('dash/topicos.html',
-                            {'form_search': searchbox},
-                            context_instance=RequestContext(request))
+    return render_to_response('dash/topicos.html', {'form_search': searchbox},
+        context_instance=RequestContext(request))
 
 
 @login_required(login_url='/accounts/user/')
 def results(request):
     searchbox = SearchBox()
-    return render_to_response('dash/results.html',
-                            {'form_search': searchbox},
-                            context_instance=RequestContext(request))
+    return render_to_response('dash/results.html', {'form_search': searchbox},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def empresa(request):
+    searchbox = SearchBox()
+    return render_to_response('dash/empresa.html', {'form_search': searchbox},
+        context_instance=RequestContext(request))
--- fanme/views.py
+++ fanme/views.py
@@ -6,8 +6,3 @@ from django.contrib.auth.decorators import login_required
 
 def redirect(request):
     return HttpResponseRedirect("/accounts/user/")
-
-
-@login_required(login_url='/accounts/login/')
-def dashboard(request):
-    return render_to_response('base.html', context_instance=RequestContext(request))
