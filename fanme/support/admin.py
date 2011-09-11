cfbaa1be0cd1955016e55eb5379973aa59605e9b
Emilio Ramirez <emilioramirez04@gmail.com>
2011-09-11 15:48:45 -0300

    migracion completada

--- fanme/support/admin.py
+++ fanme/support/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
-from fanme.support.models import Rubro, Topico, Item, Pais
-from fanme.support.models import Provincia, Localidad, Marca, Localizacion
+from fanme.support.models import Rubro, Pais, Provincia, Localidad
+from fanme.support.models import Localizacion
 
 
 admin.site.register(Rubro)
-admin.site.register(Topico)
-admin.site.register(Item)
-
 admin.site.register(Pais)
 admin.site.register(Provincia)
 admin.site.register(Localidad)
-admin.site.register(Marca)
 admin.site.register(Localizacion)
