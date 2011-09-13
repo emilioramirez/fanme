a1c44fa6bb96129ba95688cb73b086bf11983529
Matias Mu?oz <munozmatiasn@gmail.com>
2011-09-13 15:30:02 -0300

    Se agrego el error cuando no ingresa nada en el searchbox y los links a usuarios.

--- fanme/dash/forms.py
+++ fanme/dash/forms.py
@@ -7,3 +7,9 @@ class SearchBox(forms.Form):
         initial='Buscar',
         required=False,
         widget=forms.TextInput(attrs={'class': 'header-seach-form-field'}))
+
+    def clean_string(self):
+        string = self.cleaned_data['string']
+        if string == "":
+            raise forms.ValidationError("Debe ingresar una palabra")
+        return string
