270eb0aeb9e129e625802815cae6b7ac781fe711
Matias Mu?oz <munozmatiasn@gmail.com>
2011-09-16 17:13:10 -0300

    Limite la cantidad de busquedas a 8, elimine de la busqueda el mismo usuario que la estaba haciendo, y cree el html de registracion de items. Falta hacerlo bien al formulario

--- fanme/dash/views.py
+++ fanme/dash/views.py
@@ -50,28 +50,54 @@ def results(request):
     if searchbox.is_valid():
         search = searchbox.cleaned_data['string']
         user = User.objects.get(id=request.user.id)
+        #Get the 8 first items that match with the search
         items_result = Item.objects.filter(nombre__icontains=search)
+        first_items = count_items(items_result)
+        #Get the 8 first users that match with the search
         users_result_intermediate = User.objects.filter(
             Q(first_name__icontains=search) | Q(last_name__icontains=search))
+        users_result = verify_user(user, users_result_intermediate)
+        first_users = count_items(users_result)
+        #Get the 8 first organizations that match with the search
         organizations_result = Empresa.objects.filter(
         razon_social__icontains=search)
+        first_organizations = count_items(organizations_result)
+        #Get the 8 first topics that match with the search
         topics_result = Topico.objects.filter(nombre__icontains=search)
-        users_result = []
-        for users in users_result_intermediate:
-            if user.id != users.id:
-                users_result.append(users)
+        first_topics = count_items(topics_result)
     else:
         return render_to_response('dash/results.html',
             {'form_search': searchbox},
             context_instance=RequestContext(request))
     return render_to_response('dash/results.html',
         {'form_search': searchbox,
-            'items_result': items_result,
-            'users_result': users_result,
-            'organizations_result': organizations_result,
-            'topics_result': topics_result,
+            'items_result': first_items,
+            'users_result': first_users,
+            'organizations_result': first_organizations,
+            'topics_result': first_topics,
             'user': user},
         context_instance=RequestContext(request))
+
+
+def count_items(items_result):
+    list_items = []
+    i = 1
+    for items in items_result:
+        if i <= 8:
+            list_items.append(items)
+            i = i + 1
+        else:
+            break
+    return list_items
+    #print len(list_items)
+
+
+def verify_user(user, user_result_intermediate):
+    users_result = []
+    for users in user_result_intermediate:
+        if user.id != users.id:
+            users_result.append(users)
+    return users_result
 
 
 @login_required(login_url='/accounts/user/')
