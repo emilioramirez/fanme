2c356761cb35b356cc298718979b2d7a934edd50
Matias Mu?oz <munozmatiasn@gmail.com>
2011-09-17 12:33:52 -0300

    Ya se pueden crear items. Falta llamar a la pagina y ponerla bonita.

--- fanme/items/views.py
+++ fanme/items/views.py
@@ -5,7 +5,9 @@ from django.contrib.auth.decorators import login_required
 
 from fanme.dash.forms import SearchBox
 from fanme.items.models import Item
+from fanme.items.forms import ItemRegisterForm
 from fanme.accounts.models import Empresa
+from fanme.segmentation.models import Topico
 
 
 @login_required(login_url='/accounts/user/')
@@ -33,6 +35,20 @@ def empresa(request, empresa_id):
 @login_required(login_url='/accounts/user/')
 def register_item(request):
     searchbox = SearchBox()
+    if request.method == 'POST':
+        form_register = ItemRegisterForm(request.POST)
+        if form_register.is_valid():
+            nombre = form_register.cleaned_data['nombre']
+            descripcion = form_register.cleaned_data['descripcion']
+            topico = form_register.cleaned_data['topico']
+            item = Item()
+            item.nombre = nombre
+            item.descripcion = descripcion
+            item.topico = Topico.objects.get(nombre=topico)
+            item.save()
+    else:
+        form_register = ItemRegisterForm()
     return render_to_response('items/register_item.html',
-        {'form_search': searchbox},
+        {'form_search': searchbox,
+        'form_register': form_register},
         context_instance=RequestContext(request))
