f14dc414d0808b39473edcf5711c2a66a6428086
Emilio Ramirez <emilioramirez04@gmail.com>
2011-09-19 16:59:30 -0300

    eliminado un print inesesario

---
+++ fanme/items/forms.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from django import forms
+#from django.contrib.auth import authenticate
+from fanme.items.models import Marca
+from fanme.segmentation.models import Topico
+
+
+class ItemRegisterForm(forms.Form):
+    nombre = forms.CharField(
+        label='Nombre',
+        initial='Nombre',
+        required=True,
+        error_messages={'required': 'Es necesario un Nombre'},
+        widget=forms.TextInput(attrs={'class': 'item-register-name-field'}))
+    descripcion = forms.CharField(
+        label='Descripción',
+        initial='Descripción',
+        required=True,
+        error_messages={'required': 'Es necesario una descripción'},
+        widget=forms.TextInput(attrs={'class': 'item-register-name-field'}))
+    topico = forms.ModelChoiceField(
+        label='',
+        empty_label="Tópico",
+        queryset=Topico.objects.all(),
+        error_messages={'required': 'Es necesario un Tópico',
+            'invalid_choice': 'Opcion no valida'},
+        widget=forms.Select(attrs={'class': 'accounts-register-form-field'}))
+#    marca = forms.ModelChoiceField(
+#        label='',
+#        empty_label="Marca",
+#        queryset=Marca.objects.all(),
+#        error_messages={'invalid_choice': 'Opcion no valida'},
+#        widget=forms.Select(attrs={'class': 'accounts-register-form-field'}))
+
+    def clean_nombre(self):
+        data = self.cleaned_data['nombre']
+        if data == 'Nombre':
+            raise forms.ValidationError("Es necesario un Nombre")
+        return data
+
+    def clean_descripcion(self):
+        data = self.cleaned_data['descripcion']
+        if data == 'Descripcion':
+            raise forms.ValidationError("Es necesario una descripción")
+        return data
+
+
+class CommentForm(forms.Form):
+    texto = forms.CharField(
+        label='',
+        initial='Ingrese su comentario aquí',
+        required=True,
+        error_messages={'required': 'Es necesario un comentario'},
+        widget=forms.Textarea(attrs={'class': 'comment-text-add'}))
---
+++ fanme/items/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import patterns
+from fanme.items import views
+
+
+urlpatterns = patterns('',
+    #(r'^user/', views.register_user),
+    (r'^item/(?P<item_id>\d+)$', views.item),
+    (r'^empresa/(?P<empresa_id>\d+)$', views.empresa),
+    (r'^register_item/', views.register_item),
+    (r'^fan/(?P<item_id>\d+)$', views.fan),
+    (r'^unfan/(?P<item_id>\d+)$', views.unfan),
+    (r'^comment/(?P<item_id>\d+)$', views.comment),
+)
