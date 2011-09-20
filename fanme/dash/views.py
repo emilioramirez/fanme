b6b077378b1c48dcf3701103a166ee192c56a20f
Emilio Ramirez <emilioramirez04@gmail.com>
2011-09-20 10:21:22 -0300

    base de datos llenada por las chicas

---
+++ fanme/dash/views.py
@@ -0,0 +1,173 @@
+from django.http import Http404
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+from fanme.dash.forms import SearchBox
+from fanme.items.models import Item
+from fanme.accounts.models import Persona, Empresa
+from fanme.segmentation.models import Topico
+from django.db.models import Q
+
+
+@login_required(login_url='/accounts/user/')
+def dashboard(request):
+    searchbox = SearchBox()
+    try:
+        my_profile = request.user.persona
+        items_by_topics = []
+        item_topic = {}
+        for topic in my_profile.topicos.all():
+            item_topic[topic] = Item.objects.filter(topico__exact=topic)
+            items_by_topics.append(Item.objects.filter(topico__exact=topic))
+    except Persona.DoesNotExist:
+        return HttpResponseRedirect('/dash/empresa/')
+    return render_to_response('dash/dashboard.html', {'form_search': searchbox,
+        'items': items_by_topics, 'example': item_topic},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def logbook(request):
+    searchbox = SearchBox()
+    return render_to_response('dash/logbook.html', {'form_search': searchbox},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def topicos(request):
+    searchbox = SearchBox()
+    return render_to_response('dash/topicos.html', {'form_search': searchbox},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def results(request):
+    searchbox = SearchBox(request.POST)
+    if searchbox.is_valid():
+        search = searchbox.cleaned_data['string']
+        #Get the 8 first items that match with the search
+        first_items = Item.objects.filter(nombre__icontains=search)[:8]
+        #Get the 8 first users that match with the search
+        first_users = User.objects.filter(
+            Q(first_name__icontains=search) | Q(last_name__icontains=search),
+            ~Q(id=request.user.id))
+        #Get the 8 first organizations that match with the search
+        first_organizations = Empresa.objects.filter(
+        razon_social__icontains=search)[:8]
+        #Get the 8 first topics that match with the search
+        first_topics = Topico.objects.filter(nombre__icontains=search)[:8]
+    else:
+        return render_to_response('dash/results.html',
+            {'form_search': searchbox},
+            context_instance=RequestContext(request))
+    return render_to_response('dash/results.html',
+        {'form_search': searchbox,
+            'items_result': first_items,
+            'users_result': first_users,
+            'organizations_result': first_organizations,
+            'topics_result': first_topics},
+            context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def empresa(request):
+    searchbox = SearchBox()
+    return render_to_response('dash/empresa.html', {'form_search': searchbox},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def follow_user(request, user_id):
+    searchbox = SearchBox()
+    try:
+        user_to_follow = User.objects.get(id=user_id)
+    except Persona.DoesNotExist:
+        return HttpResponseRedirect('/dash/empresa/')
+    return render_to_response('dash/follow.html', {'form_search': searchbox,
+        'user_to_follow': user_to_follow},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def follow_request(request, user_id):
+    try:
+        user_to_follow = User.objects.get(id=user_id)
+        my_profile = request.user.persona
+        i_follow = my_profile.following.filter(id=user_id)
+        if i_follow:
+            return HttpResponseRedirect('/dash/logbook/{0}'.format(user_id))
+        else:
+            my_profile.following.add(user_to_follow)
+            return HttpResponseRedirect('/dash/logbook/{0}'.format(user_id))
+    except User.DoesNotExist:
+        raise Http404
+    except Persona.DoesNotExist:
+        return HttpResponseRedirect('/dash/empresa.html/')
+    return render_to_response('dash/dashboad.html',
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def logbook_user(request, user_id):
+    searchbox = SearchBox()
+    try:
+        user_logbook = User.objects.get(id=user_id)
+        my_profile = request.user.persona
+        if not my_profile.following.filter(id=user_logbook.id):
+            return HttpResponseRedirect('/dash/follow/{0}'.format(user_id))
+    except Persona.DoesNotExist:
+        return HttpResponseRedirect('/dash/empresa/')
+    return render_to_response('dash/logbook_user.html',
+        {'form_search': searchbox, 'user_logbook': user_logbook},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def my_fans_items(request):
+    searchbox = SearchBox()
+    messages = []
+    try:
+        request.user.persona
+        messages.append("Sos fan de")
+    except Persona.DoesNotExist:
+            return HttpResponseRedirect('/dash/empresa/')
+    return render_to_response('dash/my_stuff.html',
+        {'form_search': searchbox, 'messages': messages,
+        'items': request.user.persona.items.all()},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def my_comments_items(request):
+    searchbox = SearchBox()
+    messages = []
+    try:
+        request.user.persona
+        messages.append("Has comentado los siguientes items")
+    except Persona.DoesNotExist:
+            return HttpResponseRedirect('/dash/empresa/')
+    return render_to_response('dash/my_stuff.html',
+        {'form_search': searchbox, 'messages': messages,
+        'items': request.user.item_set.all().distinct()},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def following(request):
+    searchbox = SearchBox()
+    messages = ['Estas siguiendo a estos usuarios']
+    return render_to_response('dash/following.html',
+        {'form_search': searchbox, 'messages': messages},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def followers(request):
+    searchbox = SearchBox()
+    messages = ['Estos usuarios te estan siguiendo']
+    return render_to_response('dash/followers.html',
+        {'form_search': searchbox, 'messages': messages},
+        context_instance=RequestContext(request))
---
+++ fanme/items/views.py
@@ -0,0 +1,140 @@
+from django.http import Http404
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+from datetime import datetime
+
+from fanme.dash.forms import SearchBox
+from fanme.items.models import Item, Comentario, Marca
+from fanme.items.forms import ItemRegisterForm, CommentForm
+from fanme.accounts.models import Empresa, Persona
+from fanme.segmentation.models import Topico
+
+
+@login_required(login_url='/accounts/user/')
+def item(request, item_id):
+    searchbox = SearchBox()
+    comment_form = CommentForm()
+    try:
+        item = Item.objects.get(pk=item_id)
+        is_fan = request.user.persona.items.filter(nombre=item.nombre)
+        comments = item.comentario_set.all().order_by('fecha')
+    except Item.DoesNotExist:
+        raise Http404
+    return render_to_response('items/item.html', {'form_search': searchbox,
+        'item': item, 'is_fan': is_fan, 'comment_form': comment_form,
+        'comments': comments},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def empresa(request, empresa_id):
+    searchbox = SearchBox()
+    try:
+        empresa = Empresa.objects.get(pk=empresa_id)
+    except Empresa.DoesNotExist:
+        raise Http404
+    return render_to_response('items/empresa.html', {'form_search': searchbox,
+        'empresa': empresa}, context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def register_item(request):
+    searchbox = SearchBox()
+    if request.method == 'POST':
+        form_register = ItemRegisterForm(request.POST)
+        if form_register.is_valid():
+            nombre = form_register.cleaned_data['nombre']
+            descripcion = form_register.cleaned_data['descripcion']
+            topico = form_register.cleaned_data['topico']
+            marca = form_register.cleaned_data['marca']
+            item = Item()
+            item.nombre = nombre
+            item.descripcion = descripcion
+            item.topico = Topico.objects.get(nombre=topico)
+#            item.marca = Marca.objects.get(nombre=marca)
+            item.save()
+            return HttpResponseRedirect('/dash/dashboard/')
+    else:
+        form_register = ItemRegisterForm()
+    return render_to_response('items/register_item.html',
+        {'form_search': searchbox,
+        'form_register': form_register},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def fan(request, item_id):
+    searchbox = SearchBox()
+    comment_form = CommentForm()
+    messages = []
+    try:
+        item = Item.objects.get(pk=item_id)
+        is_fan = request.user.persona.items.filter(nombre=item.nombre)
+        if is_fan:
+            messages.append("Ya sos fan de {0}".format(item.nombre))
+        else:
+            request.user.persona.items.add(item)
+            is_fan = True
+            messages.append("Te has hecho fan de {0}".format(item.nombre))
+    except Item.DoesNotExist:
+        raise Http404
+    except Persona.DoesNotExist:
+        return HttpResponseRedirect('/dash/empresa.html/')
+    return render_to_response('items/item.html', {'form_search': searchbox,
+        'item': item, 'messages': messages, 'is_fan': is_fan,
+        'comment_form': comment_form},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def unfan(request, item_id):
+    searchbox = SearchBox()
+    comment_form = CommentForm()
+    messages = []
+    try:
+        item = Item.objects.get(pk=item_id)
+        request.user.persona.items.remove(item)
+        messages.append("Ya no sos fan de {0}".format(item.nombre))
+    except Item.DoesNotExist:
+        raise Http404
+    except Persona.DoesNotExist:
+        return HttpResponseRedirect('/dash/empresa.html/')
+    return render_to_response('items/item.html', {'form_search': searchbox,
+        'item': item, 'messages': messages, 'comment_form': comment_form},
+        context_instance=RequestContext(request))
+
+
+@login_required(login_url='/accounts/user/')
+def comment(request, item_id):
+    searchbox = SearchBox()
+    messages = []
+    if request.method == 'POST':
+        form_comment = CommentForm(request.POST)
+        if form_comment.is_valid():
+            try:
+                item = Item.objects.get(pk=item_id)
+                comentario_text = form_comment.cleaned_data['texto']
+                comentario = Comentario()
+                comentario.comentario = comentario_text
+                comentario.item = item
+                comentario.user = request.user
+                comentario.fecha = datetime.now()
+                comentario.save()
+                messages.append("Has comentado el producto {0}".format(
+                    item.nombre))
+                comment_form = CommentForm()
+                comments = item.comentario_set.all().order_by('fecha')
+            except Item.DoesNotExist:
+                raise Http404
+            except Persona.DoesNotExist:
+                return HttpResponseRedirect('/dash/empresa.html/')
+    else:
+        comment_form = CommentForm()
+    return render_to_response('items/item.html', {'form_search': searchbox,
+        'item': item, 'comment_form': comment_form, 'messages': messages,
+        'comments': comments},
+        context_instance=RequestContext(request))
