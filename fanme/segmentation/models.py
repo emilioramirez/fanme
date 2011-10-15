dc02d564149b0d9f14b7c70399f8889b13ff504a
Matias Mu?oz <munozmatiasn@gmail.com>
2011-10-15 10:53:03 -0300

    Agregue la parte de los mensajes entre los usuarios

---
+++ fanme/segmentation/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Topico(models.Model):
+    nombre = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=100)
+    padre = models.ForeignKey('self', null=True, blank=True)
+
+    def __unicode__(self):
+        if (self.padre != None):
+            return '{0} | {1}'.format(self.padre.__unicode__(), self.nombre)
+        else:
+            return self.nombre
+
+        return self.nombre
+
+
+class Tag(models.Model):
+    pass
+
+
+class Credibilidad(models.Model):
+    pass
+
+
+class Denuncia(models.Model):
+    fecha_denuncia = models.DateTimeField()
+    fecha_resolucion = models.DateTimeField()
+    descripcion = models.TextField(max_length=300)
