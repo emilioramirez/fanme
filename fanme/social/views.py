4c4cbafafac8ee8481d6f96c7462cf8d051a8862
Emilio Ramirez <emilioramirez04@gmail.com>
2011-10-17 16:09:16 -0300

    se arreglaron los problemas de merge, falta lo del mati

--- fanme/social/views.py
+++ fanme/social/views.py
@@ -2,16 +2,18 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from fanme.social.forms import MessageForm, MessageResponseForm
-
 from fanme.dash.forms import SearchBox
 from fanme.accounts.models import Persona
 from fanme.social.models import Mensaje
 from itertools import chain
 from operator import attrgetter
 import datetime
+
+from fanme.social.forms import EventoForm
+from fanme.social.models import Eventos, Evento
+
 
 @login_required(login_url='/accounts/user/')
 def eventos(request):
@@ -30,7 +32,7 @@ def eventos(request):
 
 
 @login_required(login_url='/accounts/user/')
-def messages(request):
+def new_evento(request):
     searchbox = SearchBox()
     if request.method == "POST":
         form = EventoForm(request.POST)
@@ -65,16 +67,93 @@ def messages(request):
 def evento(request, evento_id):
     searchbox = SearchBox()
     try:
-        mensajes_recibidos = request.user.mensajes_recibidos.all().values(
-            'user_from').distinct()
-        usuarios = []
-        for dict in mensajes_recibidos.all():
-            usuarios.append(User.objects.get(id=dict['user_from']))
-    except Persona.DoesNotExist:
-        return HttpResponseRedirect('/dash/empresa/')
-    return render_to_response('social/messages.html', {'form_search': searchbox,
-        'mensajes_recibidos': usuarios},
-        context_instance=RequestContext(request))
+        evento = Evento.objects.get(id=evento_id)
+    except Evento.DoesNotExist:
+        evento = []
+    temp = RequestContext(request, {'form_search': searchbox, 'evento': evento})
+    return render_to_response('social/evento.html', temp)
+
+
+@login_required(login_url='/accounts/user/')
+def edit_evento(request, evento_id):
+    searchbox = SearchBox()
+    try:
+        evento_db = Evento.objects.get(id=evento_id)
+    except Evento.DoesNotExist:
+        return HttpResponseRedirect('/social/eventos/')
+    if request.method == "POST":
+        form = EventoForm(request.POST, instance=evento_db)
+        list_ids = request.user.followers.values_list('user', flat=True)
+        users = User.objects.filter(id__in=list_ids)
+        form.fields["invitados"].queryset = users
+        if form.is_valid():
+            evento = form.save(commit=False)
+            try:
+                request.user.eventos
+            except Eventos.DoesNotExist:
+                eventos = Eventos()
+                eventos.creador = request.user
+                eventos.save()
+#            creador = request.user.eventos
+#            evento.creador = creador
+#            evento.fecha_creacion = datetime.now()
+            evento.save()
+            form.save_m2m()
+            return HttpResponseRedirect('/social/eventos/')
+    else:
+        form = EventoForm(instance=evento_db)
+        list_ids = request.user.followers.values_list('user', flat=True)
+        users = User.objects.filter(id__in=list_ids)
+        form.fields["invitados"].queryset = users
+    template_vars = RequestContext(request, {"form": form, "user": request.user,
+        'form_search': searchbox, 'evento': evento_db})
+    return render_to_response('social/edit_evento.html', template_vars)
+
+
+@login_required(login_url='/accounts/user/')
+def delete_evento(request, evento_id):
+    searchbox = SearchBox()
+    try:
+        evento_db = Evento.objects.get(id=evento_id)
+        evento_db.delete()
+        return HttpResponseRedirect('/social/eventos/')
+    except Evento.DoesNotExist:
+        return HttpResponseRedirect('/social/eventos/')
+    template_vars = RequestContext(request, {
+        'form_search': searchbox, 'evento': evento_db})
+    return render_to_response('social/edit_evento.html', template_vars)
+
+
+@login_required(login_url='/accounts/user/')
+def messages(request):
+    searchbox = SearchBox()
+    if request.method == "POST":
+        form = EventoForm(request.POST)
+        list_ids = request.user.followers.values_list('user', flat=True)
+        users = User.objects.filter(id__in=list_ids)
+        form.fields["invitados"].queryset = users
+        if form.is_valid():
+            evento = form.save(commit=False)
+            try:
+                request.user.eventos
+            except Eventos.DoesNotExist:
+                eventos = Eventos()
+                eventos.creador = request.user
+                eventos.save()
+            creador = request.user.eventos
+            evento.creador = creador
+            evento.fecha_creacion = datetime.now()
+            evento.save()
+            form.save_m2m()
+            return HttpResponseRedirect('/social/eventos/')
+    else:
+        form = EventoForm()
+        list_ids = request.user.followers.values_list('user', flat=True)
+        users = User.objects.filter(id__in=list_ids)
+        form.fields["invitados"].queryset = users
+    template_vars = RequestContext(request, {"form": form,
+        'form_search': searchbox})
+    return render_to_response('social/new_evento.html', template_vars)
 
 
 @login_required(login_url='/accounts/user/')
