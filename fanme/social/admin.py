4fbded068c76a058611c7f55f580dae463fa3a6e
Emilio Ramirez <emilioramirez04@gmail.com>
2011-10-18 09:29:25 -0300

    arreglado algunos templates y cosas

--- fanme/social/admin.py
+++ fanme/social/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from fanme.social.models import Evento
+from fanme.social.models import Evento, Notificacion, Mensaje
 
 admin.site.register(Evento)
+admin.site.register(Notificacion)
+admin.site.register(Mensaje)
