48ecbeac33f011cb5f05fb56aa55abdd71a07f28
Emilio Ramirez <emilioramirez04@gmail.com>
2011-10-18 18:25:56 -0300

    ultimos cambios

--- fanme/items/urls.py
+++ fanme/items/urls.py
@@ -10,5 +10,7 @@ urlpatterns = patterns('',
     (r'^fan/(?P<item_id>\d+)$', views.fan),
     (r'^unfan/(?P<item_id>\d+)$', views.unfan),
     (r'^comment/(?P<item_id>\d+)$', views.comment),
+    (r'^comment/megusta/(?P<comentario_id>\d+)$', views.me_gusta_comentario),
+    (r'^comment/denunciar/(?P<comentario_id>\d+)$', views.me_gusta_comentario),
     (r'^recomendation/(?P<item_id>\d+)$', views.recomendation),
 )
