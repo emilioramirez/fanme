d4058efcba98b7c36cbe67160e1e7f1a5cc851d8
Matias Mu?oz <munozmatiasn@gmail.com>
2011-10-18 17:42:37 -0300

    La empresa puede ver las notificaciones

---
+++ fanme/items/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls.defaults import patterns
+from fanme.items import views
+
+
+urlpatterns = patterns('',
+    #(r'^user/', views.register_user),
+    (r'^item/(?P<item_id>\d+)$', views.item),
+    (r'^empresa/(?P<empresa_id>\d+)$', views.empresa),
+    (r'^item/register/', views.register_item),
+    (r'^fan/(?P<item_id>\d+)$', views.fan),
+    (r'^unfan/(?P<item_id>\d+)$', views.unfan),
+    (r'^comment/(?P<item_id>\d+)$', views.comment),
+    (r'^recomendation/(?P<item_id>\d+)$', views.recomendation),
+)
