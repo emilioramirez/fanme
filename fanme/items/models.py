b902dd5dba2e8f5c7a9eec37cabb8dc46e13ebe1
Matias Mu?oz <munozmatiasn@gmail.com>
2011-10-25 18:05:54 -0300

    Se pueden ver las notificaciones de las empresas

---
+++ fanme/items/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from fanme.segmentation.models import Topico
+from django.contrib.auth.models import User
+
+
+class Item(models.Model):
+    nombre = models.CharField(max_length=20)
+    descripcion = models.TextField(max_length=300)
+    topico = models.ForeignKey(Topico, null=True, blank=True)
+    users_are_comment = models.ManyToManyField(User, through='Comentario')
+    cantidad_fans = models.IntegerField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class Marca(models.Model):
+    pass
+
+
+class Comentario(models.Model):
+    comentario = models.TextField(max_length=300)
+    fecha = models.DateTimeField()
+    item = models.ForeignKey(Item)
+    user = models.ForeignKey(User)
+
+
+class Recomendacion(models.Model):
+    item = models.ForeignKey(Item)
+    user_destino = models.ForeignKey(User,
+        related_name="recomendaciones_recibidas")
+    fecha = models.DateTimeField()
+    user_origen = models.ForeignKey(User,
+        related_name="recomendaciones_enviadas")
+
+    def __unicode__(self):
+        return u'{0} para {1} sobre {2}'.format(self.user_origen,
+            self.user_destino, self.item)
+
+    class Meta:
+#        verbose_name = ''
+        verbose_name_plural = "Recomendaciones"
