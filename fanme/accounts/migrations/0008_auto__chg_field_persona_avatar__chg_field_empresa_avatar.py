b5da6be448953d7562c1f68c46dd417dd9adeeee
Matias Mu?oz <munozmatiasn@gmail.com>
2011-11-08 15:56:09 -0300

    Agregue que pueda ver las notificaciones el usuario, y agregue el contador de consultas.

---
+++ fanme/accounts/migrations/0008_auto__chg_field_persona_avatar__chg_field_empresa_avatar.py
@@ -0,0 +1,121 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Changing field 'Persona.avatar'
+        db.alter_column('accounts_persona', 'avatar', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True))
+
+        # Changing field 'Empresa.avatar'
+        db.alter_column('accounts_empresa', 'avatar', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True))
+
+
+    def backwards(self, orm):
+        
+        # Changing field 'Persona.avatar'
+        db.alter_column('accounts_persona', 'avatar', self.gf('django.db.models.fields.files.ImageField')(default='', max_length=100))
+
+        # Changing field 'Empresa.avatar'
+        db.alter_column('accounts_empresa', 'avatar', self.gf('django.db.models.fields.files.ImageField')(default='', max_length=100))
+
+
+    models = {
+        'accounts.empresa': {
+            'Meta': {'object_name': 'Empresa'},
+            'avatar': ('django.db.models.fields.files.ImageField', [], {'default': "'avatares/default.png'", 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_first_time': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'razon_social': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'rubros': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['support.Rubro']", 'symmetrical': 'False'}),
+            'site': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'topicos': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['segmentation.Topico']", 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'empresa'", 'unique': 'True', 'to': "orm['auth.User']"})
+        },
+        'accounts.persona': {
+            'Meta': {'object_name': 'Persona'},
+            'avatar': ('django.db.models.fields.files.ImageField', [], {'default': "'avatares/default.png'", 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'fecha_nacimiento': ('django.db.models.fields.DateField', [], {}),
+            'following': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'followers'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_first_time': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'items': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['items.Item']", 'null': 'True', 'blank': 'True'}),
+            'sexo': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'topicos': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['segmentation.Topico']", 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'persona'", 'unique': 'True', 'to': "orm['auth.User']"})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'items.comentario': {
+            'Meta': {'object_name': 'Comentario'},
+            'comentario': ('django.db.models.fields.TextField', [], {'max_length': '300'}),
+            'denuncias': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'fecha': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['items.Item']"}),
+            'me_gusta': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'items.item': {
+            'Meta': {'object_name': 'Item'},
+            'cantidad_fans': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'descripcion': ('django.db.models.fields.TextField', [], {'max_length': '300'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'nombre': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'topico': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['segmentation.Topico']", 'null': 'True', 'blank': 'True'}),
+            'users_are_comment': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'through': "orm['items.Comentario']", 'symmetrical': 'False'})
+        },
+        'segmentation.topico': {
+            'Meta': {'object_name': 'Topico'},
+            'descripcion': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'nombre': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'padre': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['segmentation.Topico']", 'null': 'True', 'blank': 'True'})
+        },
+        'support.rubro': {
+            'Meta': {'object_name': 'Rubro'},
+            'descripcion': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'nombre': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        }
+    }
+
+    complete_apps = ['accounts']
---
+++ fanme/social/forms.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from fanme.social.models import Evento, Notificacion
+from fanme.items.models import Item
+
+
+class MessageForm(forms.Form):
+#    user_to_id = forms.CharField(
+#        label='Para',
+#        required=True,
+#        error_messages={'required': 'Es necesario un Usuario'},
+#        widget=forms.TextInput(attrs={'class': 'message-content-mati-to'}))
+    user_to_id = forms.ModelChoiceField(queryset=None)
+    mensaje = forms.CharField(
+        label='Mensaje',
+        required=True,
+        error_messages={'required': 'Es necesario ingresar un mensaje'},
+        widget=forms.Textarea(attrs={
+                'class': 'new-message-field'}))
+
+
+class MessageResponseForm(forms.Form):
+    mensaje = forms.CharField(
+        label='',
+        required=True,
+        error_messages={'required': 'Es necesario ingresar un mensaje'},
+        widget=forms.Textarea
+            (attrs={'class': 'response-message-field'}))
+
+
+class MessageQueryForm(forms.Form):
+    item = forms.ModelChoiceField(
+        label='',
+        empty_label="---",
+        queryset=Item.objects.all(),
+        error_messages={'required': 'Es necesario un Item',
+            'invalid_choice': 'Opcion no valida'},
+        widget=forms.Select(attrs={'class': 'combo-item'}))
+    consulta = forms.CharField(
+        label='Consulta',
+        required=True,
+        error_messages={'required': 'Es necesario ingresar un mensaje'},
+        widget=forms.Textarea(attrs={'class': 'new-message-field'}))
+
+
+class EventoForm(forms.ModelForm):
+
+    class Meta():
+        model = Evento
+        fields = (
+            'nombre',
+            'tipo',
+            'fecha_inicio',
+            'fecha_fin',
+            'localizacion',
+            'descripcion',
+            'invitados',
+            'imagen',
+            )
+        widgets = {
+            'fecha_inicio': forms.DateInput(attrs={
+                'class': 'evento-date-form-field field-evento-new'}),
+            'fecha_fin': forms.DateInput(attrs={
+                'class': 'evento-date-form-field field-evento-new'}),
+            'descripcion': forms.Textarea(attrs={
+                'class': 'evento-date-form-field field-evento-new'}),
+        }
+
+
+class NotificationForm(forms.ModelForm):
+
+    class Meta():
+        model = Notificacion
+        fields = (
+            'nombre',
+            'fecha_desde',
+            'fecha_expiracion',
+            'descripcion',
+            'usuarios_to',
+            )
+        widgets = {
+            'nombre': forms.TextInput(attrs={
+                'class': 'notification-name-field'}),
+            'fecha_desde': forms.DateInput(attrs={
+                'class': 'notification-name-field'}),
+            'fecha_expiracion': forms.DateInput(attrs={
+                'class': 'notification-name-field'}),
+            'descripcion': forms.Textarea(attrs={
+                'class': 'new-notification-textarea'}),
+        }
