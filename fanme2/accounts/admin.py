14fc2364bcb5c1f44a57a19990684bbd79bc6053
Emilio Ramirez <emilioramirez04@gmail.com>
2013-03-22 17:28:55 -0300

    new profiles

--- fanme2/accounts/admin.py
+++ fanme2/accounts/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from accounts.models import ProfileBase
+from accounts.models import ProfileUser
 
 
-class ProfileBaseAdmin(admin.ModelAdmin):
-    date_hierarchy = 'fecha_nacimiento'
-    list_display = ('__unicode__', 'fecha_nacimiento', 'sexo', 'is_first_time')
-    list_filter = ('sexo', 'is_first_time')
-    list_per_page = 50
-    ordering = ('user', 'fecha_nacimiento', 'sexo')
-    search_fields = ['user__first_name', 'user__last_name', 'user__email']
+#class ProfileUserAdmin(admin.ModelAdmin):
+    #list_display = ('__unicode__', 'fecha_nacimiento', 'sexo', 'is_first_time')
+    #list_filter = ('sexo', 'is_first_time')
+    #list_per_page = 50
+    #ordering = ('user', 'fecha_nacimiento', 'sexo')
+    #search_fields = ['user__first_name', 'user__last_name', 'user__email']
 
 
-admin.site.register(ProfileBase, ProfileBaseAdmin)
+admin.site.register(ProfileUser)
