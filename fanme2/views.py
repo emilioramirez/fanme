55ad1174475edcfdc1daecd6f7752caa891df3b5
Emilio Ramirez <emilioramirez04@gmail.com>
2013-04-20 19:16:04 -0300

    movidos al directorio fanme2

--- fanme2/views.py
+++
@@ -1,8 +0,0 @@
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-
-
-def home(request):
-    context = {}
-    return render_to_response('base_menu_home.html', context,
-        context_instance=RequestContext(request))
