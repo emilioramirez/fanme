a674e0f42ab80f29acb9aad220546c21c9b3528b
Emilio Ramirez <emilioramirez04@gmail.com>
2013-04-20 19:14:07 -0300

    actualizado la app django-registration para compatibilidad con django 1.5

--- fanme2/registration/__init__.py
+++ fanme2/registration/__init__.py
@@ -1,14 +1,6 @@
-VERSION = (0, 8, 0, 'alpha', 1)
+VERSION = (0, 9, 0, 'beta', 1)
+
 
 def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        if VERSION[3] != 'final':
-            version = "%s %s" % (version, VERSION[3])
-            if VERSION[4] != 0:
-                version = '%s %s' % (version, VERSION[4])
-    return version
+    from django.utils.version import get_version as django_get_version
+    return django_get_version(VERSION) # pragma: no cover
