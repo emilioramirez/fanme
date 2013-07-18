291c5a07c694ccfec2861564954bc277883cfbd0
Matias Mu?oz <munozmatiasn@gmail.com>
2013-07-18 18:12:17 -0300

    Arregle unos defases del html. Agregue el admin del core de ciertas modelos.

---
+++ fanme2/core/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+)
