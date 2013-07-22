29b76826a8de442bb20c7532c4b4b41069783299
Matias Mu?oz <munozmatiasn@gmail.com>
2013-07-21 23:49:53 -0300

    Agregando codigo para la creacion de items.

--- fanme2/core/views.py
+++ fanme2/core/views.py
@@ -1,6 +1,8 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from core.forms import ItemRegisterForm
+from core.models import Item, Topico, ItemImagen
 
 
 @login_required
@@ -11,5 +13,32 @@ def item(request):
 
 @login_required
 def create_item(request):
-    return render_to_response('item.html',
-            context_instance=RequestContext(request))
+    messages = []
+    if request.method == 'POST':
+        form_register = ItemRegisterForm(request.POST, request.FILES)
+        if form_register.is_valid():
+            nombre = form_register.cleaned_data['nombre']
+            descripcion = form_register.cleaned_data['descripcion']
+            topico = form_register.cleaned_data['topico']
+            item = Item()
+            item.nombre = nombre
+            item.descripcion = descripcion
+            item.topico = Topico.objects.get(nombre=topico)
+            item.cantidad_fans = 0
+            item.save()
+            try:
+                imagen = request.FILES['imagen']
+                itemimagen = ItemImagen()
+                itemimagen.item = item
+                itemimagen.imagen = imagen
+                itemimagen.save()
+            except KeyError:
+                print 'Excepcion en items/view.register_item'
+            messages.append("Se creo el Item exitosamente")
+            form_register = ItemRegisterForm()
+    else:
+        form_register = ItemRegisterForm()
+    return render_to_response('items/register_item.html',
+        {'form_register': form_register,
+        'messages': messages},
+        context_instance=RequestContext(request))
