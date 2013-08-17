9e45bd66d5b661615404a81622ec8ce285c6f0c1
Matias Mu?oz <munozmatiasn@gmail.com>
2013-08-17 10:47:49 -0300

    Elimine un template.

--- fanmelegacy/bussiness/views.py
+++ fanmelegacy/bussiness/views.py
@@ -8,15 +8,17 @@ from django.template import RequestContext
 def dash_planes(request):
     searchbox = SearchBox()
     messages = ['']
+    posee_plan = False
     planes = request.user.plan_empresa.all().order_by(
             'fecha_fin_vigencia')
     print planes
     if not planes:
-        messages = ['No posee planes. Por favor creee un plan.']
+        messages = ['No posee planes. Por favor seleccione un plan.']
     else:
-
-    return render_to_response('bussiness/dash_empresa.html', {'form_search': searchbox,
-        'messages': messages},
+        posee_plan = True
+        print planes
+    return render_to_response('bussiness/dash_planes.html', {'form_search': searchbox,
+        'messages': messages, 'posee_plan': posee_plan},
         context_instance=RequestContext(request))
 
 
