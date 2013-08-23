6c4c9f3943cffcaa2851e599380e472067deba53
Matias Mu?oz <munozmatiasn@gmail.com>
2013-08-23 00:43:32 -0300

    Codificacion de la eleccion de planes por parte de la empresa

--- fanmelegacy/bussiness/urls.py
+++ fanmelegacy/bussiness/urls.py
@@ -5,4 +5,5 @@ from bussiness import views
 urlpatterns = patterns('',
     #(r'^/notificacion/nuevo/$', views.new_notificacion),
     (r'^dash_planes/$', views.dash_planes),
+    (r'^elegir_plan/$', views.elegir_plan),
 )
