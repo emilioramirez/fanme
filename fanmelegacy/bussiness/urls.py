450db08a96310ed18c24e41de478e90f8cec9ad4
Matias Mu?oz <munozmatiasn@gmail.com>
2013-08-24 11:25:15 -0300

    Arreglo del bug del logeo de la empresa.

--- fanmelegacy/bussiness/urls.py
+++ fanmelegacy/bussiness/urls.py
@@ -4,6 +4,7 @@ from bussiness import views
 
 urlpatterns = patterns('',
     #(r'^/notificacion/nuevo/$', views.new_notificacion),
+    (r'^dash_empresa/$', views.dash_empresa),
     (r'^dash_planes/$', views.dash_planes),
     (r'^elegir_plan/$', views.elegir_plan),
 )
