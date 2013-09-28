a219ad227948e243161eab87debecec1e1115ac4
Matias Mu?oz <munozmatiasn@gmail.com>
2013-09-28 11:14:53 -0300

    Combinar

--- fanmelegacy/fanmelegacy/settings.py
+++ fanmelegacy/fanmelegacy/settings.py
@@ -129,6 +129,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    # Django comments
+    'django.contrib.comments',
+    'avatar',
     'accounts',
     'support',
     'dash',
