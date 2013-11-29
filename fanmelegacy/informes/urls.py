c9e30246d1e536bcab99515c96993248fda1b714
Matias Mu?oz <munozmatiasn@gmail.com>
2013-11-29 01:30:35 -0300

    Agregue el grafico de fans por topicos.

--- fanmelegacy/informes/urls.py
+++ fanmelegacy/informes/urls.py
@@ -5,4 +5,5 @@ from informes import views
 urlpatterns = patterns('',
     (r'^mypage/', views.my_admin_view),
     (r'^progreso/', views.progreso),
+    (r'^fans_por_topicos/', views.fans_por_topicos),
 )
