a24b9df0ca34e1178bcb8197a871cc576da8e31d
Matias Mu?oz <munozmatiasn@gmail.com>
2013-11-30 17:24:22 -0300

    Agrege el informe de fans por item

--- fanmelegacy/fanmelegacy/settings.py
+++ fanmelegacy/fanmelegacy/settings.py
@@ -162,6 +162,8 @@ INSTALLED_APPS = (
     'rathings'
 )
 
+ADMIN_TOOLS_MENU = 'fanmelegacy.FanMeMenu.CustomMenu'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
