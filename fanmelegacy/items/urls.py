dc60721e520f6715161a09b4186f0643960bd5e4
Emilio Ramirez <emilioramirez04@gmail.com>
2013-12-07 10:18:59 -0300

    algunos cambios

--- fanmelegacy/items/urls.py
+++ fanmelegacy/items/urls.py
@@ -9,10 +9,12 @@ urlpatterns = patterns('',
     (r'^item/register/$', views.register_item),
     (r'^fan/(?P<item_id>\d+)/$', views.fan),
     (r'^unfan/(?P<item_id>\d+)/$', views.unfan),
-    (r'^comment/(?P<item_id>\d+)/$', views.comment),
-    (r'^comment/megusta/(?P<comentario_id>\d+)/$', views.me_gusta_comentario),
-    (r'^comment/denunciar/(?P<comentario_id>\d+)/$', views.denunciar_comentario),
+
+    # (r'^comment/(?P<item_id>\d+)/$', views.comment),
+    # (r'^comment/megusta/(?P<comentario_id>\d+)/$', views.me_gusta_comentario),
+    # (r'^comment/denunciar/(?P<comentario_id>\d+)/$', views.denunciar_comentario),
     (r'^comment/eliminar/(?P<comment_id>\d+)/$', views.delete_own_comment),
+    
     (r'^recomendation/(?P<item_id>\d+)/$', views.recomendation),
     (r'^denunciar_item/(?P<item_id>\d+)/$', views.denunciar_item),
 )
