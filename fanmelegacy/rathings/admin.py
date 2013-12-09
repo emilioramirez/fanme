6d42ace62c2b0a09d5913de352c8c65d290ab87c
Matias Mu?oz <munozmatiasn@gmail.com>
2013-12-08 22:10:48 -0300

    Agregue filtros en los listados de denuncias.

--- fanmelegacy/rathings/admin.py
+++ fanmelegacy/rathings/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from rathings.models import Like, Dislike
 from rathings.custom_filter import is_item_or_comment
+from django.contrib.admin import DateFieldListFilter
 
 
 class LikeAdmin(admin.ModelAdmin):
@@ -11,8 +12,7 @@ class LikeAdmin(admin.ModelAdmin):
 class DislikeAdmin(admin.ModelAdmin):
     date_hierarchy = 'date_created'
     list_display = ('user', 'date_created', 'content_type')
-#    list_filter = ('content_type',)
-    list_filter = (is_item_or_comment,)
+    list_filter = (is_item_or_comment, ('date_created', DateFieldListFilter),)
 
 
 admin.site.register(Like, LikeAdmin)
--- fanmelegacy/segmentation/admin.py
+++ fanmelegacy/segmentation/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from segmentation.models import Topico, Denuncia, AccionAnalisisDenuncia
 from segmentation.models import EstadoAnalisisDenuncia, AnalisisDenuncia
+from django.contrib.admin import DateFieldListFilter
 
 
 class TopicoAdmin(admin.ModelAdmin):
@@ -10,8 +11,12 @@ class TopicoAdmin(admin.ModelAdmin):
     search_fields = ['nombre', 'padre__nombre']
 
 class AnalisisDenunciaAdmin(admin.ModelAdmin):
+    date_hierarchy = 'fecha_creacion'
     list_display = ('fecha_creacion', 'fecha_resolucion', 'moderador',
         'descripcion', 'estado', 'content_type', 'object_id')
+    search_fields = ['moderador__first_name', 'moderador__last_name', 'moderador__email']
+    list_filter = (('fecha_creacion', DateFieldListFilter),
+        ('fecha_resolucion', DateFieldListFilter), 'estado',)
 
 
 admin.site.register(Topico, TopicoAdmin)
