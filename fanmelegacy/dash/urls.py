7f0e48042bc3c257904a860a53177351169acb31
Emilio Ramirez <emilioramirez04@gmail.com>
2014-01-28 23:44:34 -0300

    fixed some template structure, css by bootstrap, etc

--- fanmelegacy/dash/urls.py
+++ fanmelegacy/dash/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import patterns, url
 from dash import views
 
 
 urlpatterns = patterns('',
     #(r'^user/', views.register_user),
-    (r'^dashboard/$', views.dashboard),
+    url(r'^dashboard/$', views.dashboard, name="dashboard"),
     (r'^dashboard/(?P<topic_id>\d+)$', views.dashboard_topic),
     (r'^dashboard/topic/add/(?P<topic_id>\d+)$', views.dashboard_topic_add),
     (r'^dashboard/all$', views.dashboard_all),
     (r'^following/$', views.following),
     (r'^followers/$', views.followers),
-    (r'^logbook/$', views.logbook),
+    url(r'^logbook/$', views.logbook, name="logbook"),
     (r'^logbook/(?P<user_id>\d+)$', views.logbook_user),
-    (r'^topicos/$', views.topicos),
-    (r'^results/$', views.results),
+    url(r'^topicos/$', views.topicos, name="topicos"),
+    url(r'^results/$', views.results, name="results"),
     (r'^empresa/$', views.empresa),
     (r'^fans/', views.my_fans_items),
     (r'^comments/$', views.my_comments_items),
@@ -21,7 +21,7 @@ urlpatterns = patterns('',
     (r'^recomendation/$', views.recomendaciones_recibidas),
     (r'^follow/(?P<user_id>\d+)$', views.follow_user),
     (r'^follow/request/(?P<user_id>\d+)$', views.follow_request),
-    (r'^edit_account/$', views.edit_account),
+    url(r'^edit_account/$', views.edit_account, name="edit_account"),
     (r'^edit_pass/$', views.edit_pass),
     (r'^preguntas_mas_frecuentes/$', views.preguntas_mas_frecuentes),
     (r'^temas_de_ayuda/$', views.temas_de_ayuda),
