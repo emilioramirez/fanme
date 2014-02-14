d675666489445daeef2949416626372c4fd86376
Matias Mu?oz <munozmatiasn@gmail.com>
2014-02-14 20:07:06 -0300

    Arregle un bug de pnotify.

--- fanmelegacy/static/js/pnotify-1.2.0/jquery.pnotify.js
+++ fanmelegacy/static/js/pnotify-1.2.0/jquery.pnotify.js
@@ -36,23 +36,23 @@
 				hi_hnd: "ui-icon ui-icon-grip-dotted-horizontal"
 			},
 			bootstrap: {
-			    container: "alert",
-			    notice: "alert-warning",
-			    notice_icon: "icon-exclamation-sign",
-			    info: "alert-info",
-			    info_icon: "icon-info-sign",
-			    success: "alert-success",
-			    success_icon: "icon-ok-sign",
-			    error: "alert-danger",
-			    error_icon: "icon-warning-sign",
-			    closer: "icon-remove",
-			    pin_up: "icon-pause",
-			    pin_down: "icon-play",
-			    hi_menu: "well",
-			    hi_btn: "btn",
-			    hi_btnhov: "",
-			    hi_hnd: "icon-chevron-down"
-			}
+				container: "alert",
+				notice: "alert-info",
+				notice_icon: "glyphicon glyphicon-exclamation-sign",
+				info: "alert-info",
+				info_icon: "glyphicon glyphicon-info-sign",
+				success: "alert-success",
+				success_icon: "glyphicon glyphicon-ok-sign",
+				error: "alert-danger",
+				error_icon: "glyphicon glyphicon-warning-sign",
+				closer: "glyphicon glyphicon-remove",
+				pin_up: "glyphicon glyphicon-pause",
+				pin_down: "glyphicon glyphicon-play",
+				hi_menu: "well",
+				hi_btn: "btn btn-default",
+				hi_btnhov: "",
+				hi_hnd: "glyphicon glyphicon-chevron-down"
+			    }
 		};
 	// Set global variables.
 	var do_when_ready = function(){
